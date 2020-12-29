397b21cf602dab423e38bb139375d282cc2dc4b8
kouz <lastpuzly@gmail.com>
2020-12-29 16:44:56 +0900

    chore: 프로젝트 초기 설정

---
+++ quna/src/test/java/com/ktpt/quna/QunaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ktpt.quna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QunaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
