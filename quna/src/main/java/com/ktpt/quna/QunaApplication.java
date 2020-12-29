5a6f0c65362f4d0f0b2d8947847e3b8dfdd6c651
Gyeong-Jun Kim <lastpuzly@gmail.com>
2020-12-29 16:49:55 +0900

    chore: 프로젝트 초기 설정 (#2)

---
+++ quna/src/main/java/com/ktpt/quna/QunaApplication.java
@@ -0,0 +1,12 @@
+package com.ktpt.quna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QunaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QunaApplication.class, args);
+    }
+}
