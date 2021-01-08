a65a234fc93bd260216bbc1d8c7e46247a269f7c
toneyparky <qkrtmddhks95@gmail.com>
2021-01-08 13:20:35 +0900

    feat: Question 삭제 기능 구현

--- quna/src/main/java/com/ktpt/quna/presentation/QuestionController.java
+++ quna/src/main/java/com/ktpt/quna/presentation/QuestionController.java
@@ -1,9 +1,10 @@
 package com.ktpt.quna.presentation;
 
 import java.net.URI;
-import java.time.LocalDateTime;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +18,22 @@ import com.ktpt.quna.application.dto.QuestionResponse;
 @RestController
 public class QuestionController {
 
+    private final QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
     @PostMapping
     public ResponseEntity<QuestionResponse> create(@RequestBody QuestionRequest request) {
         QuestionResponse response = questionService.create(request);
         return ResponseEntity.created(URI.create("/questions/" + response.getId()))
                 .body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        questionService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
