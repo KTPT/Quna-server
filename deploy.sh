7f160675547a8ac4d07f3291b48c6484bcbe23f1
toneyparky <qkrtmddhks95@gmail.com>
2021-01-28 00:33:41 +0900

    chore: pwd 추가

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ docker rm $(docker container ps -a -q --filter ancestor=back)
 
 docker rmi $(docker images --filter=reference='back:*' -qa)
 
+pwd
+
 docker build --tag back .
 
 docker run -d --name back-container back
